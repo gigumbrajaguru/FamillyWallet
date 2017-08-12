61f3d082d9700a8bbed03f49ac62646e33e1ce2a
Gihansg <tharukakumarasinghesg@gmail.com>
2017-08-12 21:35:15 +0530

    Removed piechart and setted the dynamic array to single fragment currently working on assigning realtime data to the array

--- app/src/main/java/ccpe001/familywallet/summary/sumMain.java
+++ app/src/main/java/ccpe001/familywallet/summary/sumMain.java
@@ -18,25 +18,42 @@ import java.util.List;
 
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import ccpe001.familywallet.R;
+
+import static ccpe001.familywallet.R.mipmap.category;
 
 public class sumMain extends Fragment {
 
     //meeka athule charts tika karapn gihan, navigation drawer eka 'Reports' walin meekaai display weene
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        View viewt = inflater.inflate(R.layout.sum_main, container, false);
-        Button loader = (Button) viewt.findViewById(R.id.ldbtn);
-        loader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View viewt) {
-                Intent test = new Intent("ccpe001.familywallet.summary.pieChart");
-                startActivity(test);
+            View view = inflater.inflate(R.layout.sum_main, container, false);
+            //static array declaration
+            float transac[] = {750.0f, 150.0f, 200.0f};
+            String category[] = {"food", "other", "fees"};
+            //Pie chart method to populate
+            List<PieEntry> pieEntries = new ArrayList<>();
+            for (int i = 0; i < transac.length; i++) {
+                pieEntries.add(new PieEntry(transac[i], category[i]));
             }
-        });
 
-        return viewt;
+            PieDataSet dataSet = new PieDataSet(pieEntries, "Transactions Done");
+            dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+            PieData data = new PieData(dataSet);
+            PieChart charts;
+            charts = (PieChart) view.findViewById(R.id.chart);
+            charts.setData(data);
+            charts.animateY(1000);
+            charts.invalidate();
+            return view;
+        }
+
     }
 
-}
